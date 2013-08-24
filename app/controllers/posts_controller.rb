48fb087a64236a0d309507529c3dd8d2f076d080
Simon <zwsimon@gmail.com>
2013-08-24 20:57:27 +0800

    0824 update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+end
