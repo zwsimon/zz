8dde7679f3000949e2c58b0a37c42cad413a5828
Simon <zwsimon@gmail.com>
2013-08-26 00:41:43 +0800

    tutorial is done! - doesn't work on server

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-  has_many :comments, dependent: :destroy             # just like a sentence
   validates :title, presence: true,
             length: { minimum: 5 }
 end
